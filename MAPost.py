88d17a009d9a36f9bca0a6c7559eaffbe95370fa
katsuyut <katsuyut@andrew.cmu.edu>
2019-11-01 11:26:01 -0400

    update

--- MAPost.py
+++ MAPost.py
@@ -6,7 +6,9 @@ from ase.io import read, write
 import matplotlib.pyplot as plt
 from ase.io.trajectory import Trajectory, TrajectoryWriter
 from ase.visualize import view
-from ktms import *
+from scipy.spatial.qhull import QhullError
+from pymatgen.analysis.local_env import VoronoiNN
+from MAUtil import *
 from MAInit import *
 
 
@@ -71,3 +73,213 @@ def getadsposinfo(file, flag=0):
         return igroups, iposlis, rgroups, rposlis
     
     return igroups, iposlis
+
+
+def fingerprint_adslab_multiads(atoms):
+    '''
+    This function will fingerprint a slab+adsorbate atoms object for you.
+    It only  with multiple adsorbates.
+    Arg:
+        atoms   `ase.Atoms` object to fingerprint. The slab atoms must be
+                tagged with non-zero integers and adsorbate atoms must be
+                tagged with zero. This function also assumes that the
+                first atom in each adsorbate is the binding atom (e.g.,
+                of all atoms with tag==1, the first atom is the binding;
+                the same goes for tag==2 and tag==3 etc.).
+    Returns:
+        fingerprint A dictionary whose keys are:
+                        coordination            A string indicating the
+                                                first shell of
+                                                coordinated atoms
+                        neighborcoord           A list of strings
+                                                indicating the coordination
+                                                of each of the atoms in
+                                                the first shell of
+                                                coordinated atoms
+                        nextnearestcoordination A string identifying the
+                                                coordination of the
+                                                adsorbate when using a
+                                                loose tolerance for
+                                                identifying "neighbors"
+    '''
+    # Replace the adsorbate[s] with a single Krypton atom at the first binding
+    # site. We need the Krypton there so that pymatgen can find its
+    # coordination.
+    atoms, binding_positions = removemolecule(atoms, 'CO')
+    for i in reversed(range(len(binding_positions))):
+        atoms += Atoms('Kr', positions=[binding_positions[i]])
+    Krypton_indexes = []
+    for atom in atoms:
+        if atom.symbol == 'Kr':
+            Krypton_indexes.append(atom.index)
+    struct = AseAtomsAdaptor.get_structure(atoms)
+    results = []
+    try:
+        for Krypton_index in Krypton_indexes:
+            # We have a standard and a loose Voronoi neighbor finder for various
+            # purposes
+            vnn = VoronoiNN(allow_pathological=True, tol=0.6, cutoff=10) # originally tol=0.8
+            vnn_loose = VoronoiNN(allow_pathological=True, tol=0.2, cutoff=10)
+
+            # Find the coordination
+            nn_info = vnn.get_nn_info(struct, n=Krypton_index)
+            coordination, cindex = __get_coordination_string_mod(nn_info)
+
+            # Find the neighborcoord
+            neighborcoord = []
+            for neighbor_info in nn_info:
+                if neighbor_info['site'].species_string != 'Kr': # Need this to exclude just neighbor adsorbate to recognize as bonding atom
+                    # Get the coordination of this neighbor atom, e.g., 'Cu-Cu'
+                    neighbor_index = neighbor_info['site_index']
+                    neighbor_nn_info = vnn_loose.get_nn_info(struct, n=neighbor_index)
+                    neighbor_coord, cc_indexes = __get_coordination_string_mod(neighbor_nn_info)
+                    # Prefix the coordination of this neighbor atom with the identity
+                    # of the neighber, e.g. 'Cu:Cu-Cu'
+                    neighbor_element = neighbor_info['site'].species_string
+                    neighbor_coord_labeled = neighbor_element + ':' + neighbor_coord
+                    neighborcoord.append(neighbor_coord_labeled)
+
+            # Find the nextnearestcoordination
+            nn_info_loose = vnn_loose.get_nn_info(struct, n=Krypton_index)
+            nextnearestcoordination = __get_coordination_string_mod(nn_info_loose)
+            
+            results.append({'adsorbate_index': Krypton_index,
+                            'coordination': [coordination, cindex],
+                            'neighborcoord': [neighborcoord, cc_indexes],
+                            'nextnearestcoordination': nextnearestcoordination})
+        return results
+        
+    # If we get some QHull or ValueError, then just assume that the adsorbate desorbed
+    except (QhullError, ValueError, UnboundLocalError):
+        results.append({'coordination': '',
+                'neighborcoord': '',
+                'nextnearestcoordination': ''})
+        return results
+    
+                           
+def __get_coordination_string_mod(nn_info):
+    '''
+    This helper function takes the output of the `VoronoiNN.get_nn_info` method
+    and gives you a standardized coordination string.
+    Arg:
+        nn_info     The output of the
+                    `pymatgen.analysis.local_env.VoronoiNN.get_nn_info` method.
+    Returns:
+        coordination    A string indicating the coordination of the site
+                        you fed implicitly through the argument, e.g., 'Cu-Cu-Cu'
+    '''
+    coordinated_atoms = [neighbor_info['site'].species_string
+                         for neighbor_info in nn_info
+                         if neighbor_info['site'].species_string != 'Kr']
+    coordination = '-'.join(sorted(coordinated_atoms))
+    
+    coordinated_indexes = [neighbor_info['site_index']
+                         for neighbor_info in nn_info
+                         if neighbor_info['site'].species_string != 'Kr']
+
+    return coordination, coordinated_indexes
+
+def get_bonding_matrix(atoms):
+    '''
+    This function will fingerprint a slab+adsorbate atoms object for you.
+    It only  with multiple adsorbates.
+    Arg:
+        atoms   `ase.Atoms` object to fingerprint. The slab atoms must be
+                tagged with non-zero integers and adsorbate atoms must be
+                tagged with zero. This function also assumes that the
+                first atom in each adsorbate is the binding atom (e.g.,
+                of all atoms with tag==1, the first atom is the binding;
+                the same goes for tag==2 and tag==3 etc.).
+    Returns:
+        fingerprint A dictionary whose keys are:
+                        coordination            A string indicating the
+                                                first shell of
+                                                coordinated atoms
+                        neighborcoord           A list of strings
+                                                indicating the coordination
+                                                of each of the atoms in
+                                                the first shell of
+                                                coordinated atoms
+                        nextnearestcoordination A string identifying the
+                                                coordination of the
+                                                adsorbate when using a
+                                                loose tolerance for
+                                                identifying "neighbors"
+    '''
+    # Replace the adsorbate[s] with a single Krypton atom at the first binding
+    # site. We need the Krypton there so that pymatgen can find its
+    # coordination.  
+    atoms, binding_positions = removemolecule(atoms, 'CO')
+    nads = len(binding_positions)
+    for i in reversed(range(nads)):
+        atoms += Atoms('Kr', positions=[binding_positions[i]])
+    b_mat = np.zeros([len(atoms), len(atoms)])
+    Krypton_indexes = []
+    for atom in atoms:
+        if atom.symbol == 'Kr':
+            Krypton_indexes.append(atom.index)
+    struct = AseAtomsAdaptor.get_structure(atoms)
+    
+    try:
+        for atom in atoms:
+            # We have a standard and a loose Voronoi neighbor finder for various
+            # purposes
+            vnn = VoronoiNN(allow_pathological=True, tol=0.6, cutoff=10) # originally tol=0.8
+            vnn_loose = VoronoiNN(allow_pathological=True, tol=0.2, cutoff=10)
+
+            # Find the coordination
+            if atom.symbol == 'Kr':
+                nn_info = vnn.get_nn_info(struct, n=atom.index)
+                coordination, cindexes = __get_coordination_string_mod(nn_info)
+            else:
+                nn_info = vnn_loose.get_nn_info(struct, n=atom.index)
+                coordination, cindexes = __get_coordination_string_mod(nn_info)
+
+            for cindex in cindexes:
+                b_mat[atom.index][cindex] = 1
+                b_mat[cindex][atom.index] = 1
+            
+        return b_mat, nads
+        
+    # If we get some QHull or ValueError, then just assume that the adsorbate desorbed
+    except (QhullError, ValueError):
+        return None
+    
+def get_repeated_atoms(atoms, repeat):
+    cpatoms = copy.deepcopy(atoms)
+    for i in reversed(range(len(cpatoms))):
+        if cpatoms[i].tag == 3 or cpatoms[i].tag == 4:
+            cpatoms.pop(i)
+    cpatoms = cpatoms.repeat([repeat,repeat,1])
+    return cpatoms
+
+def get_number_matrix(b_mat, nads, repeat):
+#     b_matlis = []
+#     sumb_matlis = []
+    newb_mat = copy.deepcopy(b_mat)
+#     sumb_mat = 0
+    done = np.zeros([nads//repeat**2])
+    results = []
+    i = 2
+    while True:
+        newb_mat = newb_mat @ b_mat
+#         b_matlis.append(newb_mat)
+#         sumb_mat = sumb_mat + newb_mat
+#         sumb_matlis.append(sumb_mat)
+        newb_matCO = (newb_mat - np.diag(np.diag(newb_mat)))[-nads:,-nads:] # extract related adsorbate and non-diagonal terms
+        newb_matCO = newb_matCO[nads//repeat**2*4:nads//repeat**2*5] # examinimg only center one is sufficient
+
+        for j in range(nads//repeat**2):
+            if done[j] == 0:
+                if not (newb_matCO[j] == 0).all():
+                    done[j] = 1
+                    nnearestbonding = int(np.max(np.sum(newb_matCO[j:nads:nads//repeat**2,:], axis=1)))
+#                     print('adsorbate {} has {} nearest adsorbate at {}th neighbor'.format(j,nnearestbonding,i))
+#                     print(newb_matCO[j:nads:nads//repeat**2,:])
+                    results.append([j,i,nnearestbonding])
+                
+#         print('')
+        i += 1
+        if (done == 1).all() or i==6:
+            break
+    return np.array(results) # [adsorbate_index, distance, # of nearest adsorbate]
