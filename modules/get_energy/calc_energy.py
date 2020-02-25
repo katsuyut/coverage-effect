d0cb7c4b96f16f6a0bfa08b85ffc26b6f53acc4c
katsuyut <katsuyut@andrew.cmu.edu>
2020-02-24 20:57:26 -0500

    update

--- modules/get_energy/calc_energy.py
+++ modules/get_energy/calc_energy.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys, copy, time
 from MAUtil import *
 from MACalc import *
+from custodian.custodian import Custodian
+from custodian.vasp.handlers import VaspErrorHandler, UnconvergedErrorHandler, MaxForceErrorHandler
 
 start = time.time()
 
@@ -36,6 +38,22 @@ if query(name, env) != 'No file':
     print(atoms.get_potential_energy())
     e_atoms = 'Already in directory'
 else:
+    atoms.set_calculator(vasptags)
+    e_atoms = get_energy(atoms, name[0:-5], vasptags, env)
+
+
+### use custodian and if error is found restart ###
+handlers = [VaspErrorHandler(), UnconvergedErrorHandler(), MaxForceErrorHandler()]
+
+flag = False
+for handler in handlers:
+    if handler.check():
+        flag = True
+        handler.correct()
+
+if flag:
+    vasptags = Vasp(restart=True)
+    atoms = vasptags.get_atoms()
     atoms.set_calculator(vasptags)
     e_atoms = get_energy(atoms, name[0:-5], vasptags, env)
 
