981d8d0a5d70c049dbdfac7e65246d0e84480379
katsuyut <katsuyukitomita.1229@gmail.com>
2020-03-04 14:57:32 -0500

    update

--- modules/make_initial_surface.py
+++ modules/make_initial_surface.py
@@ -4,7 +4,7 @@ from MACalc import *
 elements = ['Cu', 'Pt', 'Ag', 'Pd', 'Au', 'Ni', 'Al', 'Rh']
 # xcs = ['RPBE', 'RPBE-D2', 'vdW-DF', 'optB88-vdW', 'vdW-DF2', 'BEEF-vdW']
 xcs = ['RPBE']
-faces = ['100', '111', '110']
+faces = ['100', '111', '110', '211']
 unitlengths = [1, 2, 3]
 adsorbate = 'CO'
 # maxmole = []
@@ -18,6 +18,7 @@ for ele in elements:
                bare.make_surface_pymatgen(face, unit, 4)
 
 ### other than 221
+faces = ['100', '111', '110']
 for ele in elements:
     for xc in xcs:
         for face in faces:
@@ -32,10 +33,11 @@ for ele in elements:
                 adsorbed.make_surface(maxmole=molenum, mindist=2.5)
                 adsorbed.write_trajectory()
 
-### for 221
+### for 211
+faces = ['211']
 for ele in elements:
     for xc in xcs:
-        for face in ['221']:
+        for face in ['211']:
             name = ele + '_' + face + '_u' + str(2) + '_' + xc
             adsname = adsorbate + '_' + xc
             adsorbed = make_adsorbed_surface(name, adsname)
